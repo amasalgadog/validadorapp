9ad13e5ce75335e9215a1bedbaf5b6c4a45963f6
gato4d <alvaro.asalgado@ug.uchile.cl>
2025-06-18 22:57:56 -0400

    Se modifica App.java para según comentarios de sonarqube scanner

--- src/main/java/com/equipo/validador/App.java
+++ src/main/java/com/equipo/validador/App.java
@@ -1,16 +1,11 @@
 package com.equipo.validador;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        String usuario = "admin";
-        String password = "123456";
-        if (usuario.equals("admin")){
+        String usuario = System.getenv("APP_USER");
+        if ("admin".equals(usuario)){
             System.out.println("Bienvenido, administrador!");
         }
     }
